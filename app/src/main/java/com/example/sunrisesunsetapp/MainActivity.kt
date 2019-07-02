910f947b85784a8b16d5192cfe29865d2e35b1e7
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2019-07-01 23:56:11 -0400

    Initial layout
    layout in activity_main.xml

--- app/src/main/java/com/example/sunrisesunsetapp/MainActivity.kt
+++ app/src/main/java/com/example/sunrisesunsetapp/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.sunrisesunsetapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    protected fun getWeatherInfo(view:View){
+        val appid = "10cd2fe68287d32ec0950627dcac7499"
+        var base_url = "http://api.openweathermap.org/data/2.5/weather?"
+        var location = "London, UK"
+        var url = buildURL(base_url, location, appid)
+    }
+
+    fun buildURL(base_url:String, location:String, appid:String):String{
+        return base_url + "q=" + location + "&APPID=" + appid
     }
 }
